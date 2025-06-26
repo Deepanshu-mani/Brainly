2c8403b336110d7191ab220b341f1222d45ef035
Mani <deepanshukumar1542004@gmail.com>
2025-06-26 18:33:51 +0530

    optimized for local

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL="http://localhost:3000"
+// export const BACKEND_URL="http://localhost:3000"
   
 
-  // "https://brainly-backendd.onrender.com";
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL!;
