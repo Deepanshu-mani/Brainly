ecafb26c756518dfd9059ddddde1df16da2fe896
Mani <deepanshukumar1542004@gmail.com>
2025-06-26 18:49:24 +0530

    resolving errors

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 // export const BACKEND_URL="http://localhost:3000"
   
 
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL!;
+console.log("VITE_BACKEND_URL =", import.meta.env.VITE_BACKEND_URL);
